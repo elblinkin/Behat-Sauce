4d4c60c58a7a08f5273ff8a9e01b04e2eae06964
Laura Beth Lincoln <llincoln@etsy.com>
2012-04-23 20:19:14 -0400

    Send job result to Sauce at the end of the test run.

--- src/Behat/Sauce/Context/SauceContext.php
+++ src/Behat/Sauce/Context/SauceContext.php
@@ -13,14 +13,21 @@ use InvalidArgumentException;
 
 class SauceContext extends BaseMinkContext {
 
+    const PASSED = 0;
+    const PENDING = 2;
+    const UNDEFINED = 3;
+    const FAILED = 4;
+
     private static $mink;
 
     private $parameters;
+    private $local;
 
     public function __construct(
         array $parameters
     ) {
         $this->parameters = $parameters;
+        $this->local = false;
         if (!array_key_exists('show_cmd', $this->parameters)) {
             $this->parameters['show_cmd'] = $this->getDefaultShowCmd();
         }
@@ -70,6 +77,7 @@ class SauceContext extends BaseMinkContext {
         $local
     ) {
         $mink = $this->getMink();
+        $this->local = $local;
         if (!$mink->hasSession('selenium')) {
             if ($local) {
                 $this->registerLocalSession($browser);
@@ -113,14 +121,8 @@ class SauceContext extends BaseMinkContext {
         $mink = $this->getMink();
         $host = $this->getParameter('host');
         $port = $this->getParameter('port');
-        $username = $this->getParameter('username');
-        if ($username === null) {
-            throw new InvalidArgumentException('Must set "username" in behat.yml');
-        }
-        $access_key = $this->getParameter('access_key');
-        if ($access_key === null) {
-            throw new InvalidArgumentException('Must set "access_key" in behat.yml');
-        }
+        $username = $this->getUsername();
+        $access_key = $this->getAccessKey();
         $name = $this->getParameter('name');
         $browser = sprintf(
             '{
@@ -173,6 +175,36 @@ class SauceContext extends BaseMinkContext {
     }
 
     /**
+     * Send the job result to SauceLabs.
+     *
+     * @AfterScenario
+     */
+    public function integrateJobResults($event) {
+        if ($this->local) {
+            return;
+        }
+        $result = $event->getResult();
+        switch ($result) {
+            case self::PASSED:
+                $result = 'true';
+                break;
+            case self::PENDING:
+            case self::UNDEFINED:
+            case self::FAILED:
+                $result = 'false';
+                break;
+            default:
+                throw new UnexpectedValueException($result);
+        };
+        $this->modifySauceJob(
+            sprintf(
+                '{"passed": %s}',
+                $result
+            )
+        );
+    }
+
+    /**
      * Stops started Mink sessions.
      *
      * @AfterSuite
@@ -180,6 +212,57 @@ class SauceContext extends BaseMinkContext {
     public static function stopMinkSessions() {
         self::$mink->stopSessions();
         self::$mink = null;
+    }
+
+    private function getUsername() {
+        $username = $this->getParameter('username');
+        if ($username === null) {
+            throw new InvalidArgumentException('Must set "username" in behat.yml');
+        }
+        return $username;
+    }
+
+    private function getAccessKey() {
+        $access_key = $this->getParameter('access_key');
+        if ($access_key === null) {
+            throw new InvalidArgumentException('Must set "access_key" in behat.yml');
+        }
+        return $access_key;
+    }
+
+    private function getSessionId() {
+        return $this->getMink()
+            ->getSession()
+            ->getDriver()
+            ->getBrowser()
+            ->getEval('selenium.sessionId');
+    }
+
+    private function modifySauceJob($payload) {
+        $username = $this->getUsername();
+        $access_key = $this->getAccessKey();
+        $session_id = $this->getSessionId();
+        $ch = curl_init(
+            sprintf(
+                'https://%s:%s@saucelabs.com/rest/v1/%s/jobs/%s',
+                $username,
+                $access_key,
+                $username,
+                $session_id
+            )
+        );
+        $length = strlen($payload);
+        $fh = fopen('php://memory', 'rw');
+        fwrite($fh, $payload);
+        rewind($fh);
+
+        curl_setopt($ch, CURLOPT_PUT, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_INFILE, $fh);
+        curl_setopt($ch, CURLOPT_INFILESIZE, $length);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($ch);
+        curl_close($ch);
     }
 
     /**
